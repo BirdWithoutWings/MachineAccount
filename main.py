b76f43837f7f7e93ca16eb84d030b7bd37e59957
xuyk <158129199@qq.com>
2021-03-19 22:34:24 +0800

    update

--- main.py
+++ main.py
@@ -112,19 +112,4 @@ if __name__ == "__main__":
     # for name in allname:
     #     print(name)
 
-# def traverse(f):
-#     fs = os.listdir(f)
-#     for f1 in fs:
-#         tmp_path = os.path.join(f, f1)
-#         if not os.path.isdir(tmp_path):
-#             print('文件: %s' % tmp_path)
-#         else:
-#             print('文件夹：%s' % tmp_path)
-#             traverse(tmp_path)
-#
-#
-# path = 'E:/3、台账专用包/原桌面文件夹/执法业务台账'
-# traverse(path)
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
